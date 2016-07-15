e7a1145ffbc45d21d364ace19183f18fe652287a
Eric McCormick <edm00se@gmail.com>
2016-07-15 14:26:06 -0500

    added loading visual improvements

--- javascripts/app.js
+++ javascripts/app.js
@@ -2,12 +2,13 @@
   // define Vue.js app
   new Vue({
     el: '#app',
-    data: { },
+    data: { loading: true },
     ready: function() {
       // GET request
       this.$http({url: 'links.json', method: 'GET'}).then(function (response) {
           // set data on vm
           this.$set('linksAr', response.data.linksAr);
+          this.loading = false;
           //console.table(response.data.linksAr);
       }, function (response) {
         this.$set('linksAr', null);
